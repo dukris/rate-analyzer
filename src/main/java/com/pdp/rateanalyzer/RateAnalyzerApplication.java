9d6ba89b16f51dc37b73092e79b6cd1d2b1a0827
anna.izmaylovich <anna.izmaylovich@x5.ru>
2025-02-28 14:54:40 +0300

    Initial commit

---
+++ src/main/java/com/pdp/rateanalyzer/RateAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.pdp.rateanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateAnalyzerApplication.class, args);
+	}
+
+}
