0fb446ced5e743d769be6471cdf153cada0cd3a2
anna.izmaylovich <anna.izmaylovich@x5.ru>
2025-03-04 12:49:30 +0300

    #4: active profile for test

--- src/test/java/com/pdp/rateanalyzer/api/PreferenceControllerTest.java
+++ src/test/java/com/pdp/rateanalyzer/api/PreferenceControllerTest.java
@@ -23,8 +23,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @WebMvcTest(controllers = PreferenceController.class)
 class PreferenceControllerTest {
 
